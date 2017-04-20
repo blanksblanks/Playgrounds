9b6d87fcefba07505182f5588748f9aa66ec6d05
Nina Baculinao <nb2406@columbia.edu>
2017-04-20 17:07:25 -0400

    Disemvowel a given string

--- AnAppleADay.playground/Contents.swift
+++ AnAppleADay.playground/Contents.swift
@@ -21,3 +21,19 @@
 
 import Foundation
 
+/// 4/20/17
+
+func disemvowel(_ s: String) -> String {
+    func isVowel(s: String) -> Bool {
+        return !["a","e","i","o","u"].contains(s.lowercased())
+    }
+    return Array(s.characters)
+        .map{ String.init($0) }
+        .filter(isVowel)
+        .joined()
+}
+
+expect(disemvowel("you are so weird!")).to(equal("y r s wrd!"))
+
+
+
