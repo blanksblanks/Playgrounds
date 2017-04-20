0ce5c072503132ab9a12897e2236b4bd9a36a8f1
Nina Baculinao <nb2406@columbia.edu>
2017-04-20 17:05:56 -0400

    Start a new playground for simple algo practice

---
+++ AnAppleADay.playground/Contents.swift
@@ -0,0 +1,23 @@
+/**
+ Nimble matchers:
+ beAKindOf
+ beAnInstanceOf
+ beCloseTo(_, within)
+ beEmpty
+ beginWith
+ endWith
+ beGreaterThan
+ beGreaterThanOrEqualTo etc.
+ beIdenticalTo
+ equal
+ beTrue
+ beFalsy
+ beNil
+ beVoid
+ contain
+ haveCount
+ match(regex)
+ */
+
+import Foundation
+
